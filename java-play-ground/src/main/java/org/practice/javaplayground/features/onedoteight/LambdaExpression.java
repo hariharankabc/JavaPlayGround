e7b48c5712feec833ec4e5a2f3f38a3fd7b8af50
HAK <67498648+hariharankabc@users.noreply.github.com>
2021-06-13 16:52:09 +0530

    Update LambdaExpression.java

--- java-play-ground/src/main/java/org/practice/javaplayground/features/onedoteight/LambdaExpression.java
+++ java-play-ground/src/main/java/org/practice/javaplayground/features/onedoteight/LambdaExpression.java
@@ -1,5 +1,14 @@
 package org.practice.javaplayground.features.onedoteight;
 
-public class LambdaExpression {
+import java.util.ArrayList;
 
+public class LambdaExpression {
+	public void exampleLambdaExpression(){
+		   ArrayList<Integer> numbers = new ArrayList<Integer>();
+		    numbers.add(5);
+		    numbers.add(9);
+		    numbers.add(8);
+		    numbers.add(1);
+		    numbers.forEach( (n) -> { System.out.println(n); } ); 
+		  }
 }
