8dca305124af334e54668d54577d8991ef81fa36
HAK <67498648+hariharankabc@users.noreply.github.com>
2021-06-20 20:17:54 +0530

    Update FunctionalInterface.java

--- java-play-ground/src/main/java/org/practice/javaplayground/features/onedoteight/FunctionalInterface.java
+++ java-play-ground/src/main/java/org/practice/javaplayground/features/onedoteight/FunctionalInterface.java
@@ -2,4 +2,8 @@ package org.practice.javaplayground.features.onedoteight;
 
 public interface FunctionalInterface{
     public void printFunctionalInterface();
+    default void show()
+    {
+      System.out.println("Default Method Executed");
+    }
 }
